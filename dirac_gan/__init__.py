629522f31eaeeb9649bafbcccf59877563a83b7a
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-06-28 10:18:57 +0200

    Manage imports

--- dirac_gan/__init__.py
+++ dirac_gan/__init__.py
@@ -1,3 +1,8 @@
 # Import generator and discriminator
 from dirac_gan.generator import get_generator as Generator
 from dirac_gan.discriminator import get_discriminator as Discriminator
+# Import losses
+from dirac_gan.loss import GANLossGenerator, GANLossDiscriminator, NSGANLossGenerator, NSGANLossDiscriminator, \
+    LSGANLossGenerator, LSGANLossDiscriminator, WassersteinGANLossGenerator, WassersteinGANLossDiscriminator, \
+    WassersteinGANLossGPGenerator, WassersteinGANLossGPDiscriminator, HingeGANLossGenerator, HingeGANLossDiscriminator, \
+    R1, R2
