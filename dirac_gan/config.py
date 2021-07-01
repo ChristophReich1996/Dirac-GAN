78c12a300bb824344beede8dc795ace4ccecb8bc
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-07-01 22:29:05 +0200

    Add hyperparameter dict

--- dirac_gan/config.py
+++ dirac_gan/config.py
@@ -1,14 +1,5 @@
 from typing import Dict, Any
 
-import torch.nn as nn
-import functools
-
-GENERATOR_CONFIG: Dict[str, Any] = {
-    "features": ((8, 8), (8, 16), (16, 32), (32, 2)),
-    "activation": functools.partial(nn.LeakyReLU, 0.2, True)
-}
-
-DISCRIMINATOR_CONFIG: Dict[str, Any] = {
-    "features": ((2, 8), (8, 16), (16, 32), (32, 1)),
-    "activation": functools.partial(nn.LeakyReLU, 0.2, True)
+HYPERPARAMETERS: Dict[str, Any] = {
+    "batch_size": 128
 }
